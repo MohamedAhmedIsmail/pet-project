579476e16411342fd0ece348f4b163060e66eb93
MohamedIsmail95 <mohamedesmail_3333@yahoo.com>
2025-05-05 20:16:34 +0300

    adding new files from API master course

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        \App\Models\User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'mohamed@gmail.com',
-            'password' => bcrypt('123456'),
-        ]);
+        $users = \App\Models\User::factory(10)->create();
+        \App\Models\Ticket::factory(100)->recycle($users)->create();
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'mohamed@gmail.com',
+        //     'password' => bcrypt('123456'),
+        // ]);
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\V1\TicketController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +15,11 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('/login',[AuthController::class, 'login']);
+Route::post('/register',[AuthController::class, 'register']);
+Route::middleware('auth:sanctum')->post('/logout',[AuthController::class,'logout']);
+
+Route::apiResource('/tickets',TicketController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
